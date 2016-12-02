0ac5f88e8a1fe248a98542d07e24dc60fc28917f
dandes3 <hopeisforsissies@icloud.com>
2016-12-02 16:32:27 -0500

    Added most files

--- hash.hpp
+++ hash.hpp
@@ -1,1 +1,26 @@
 //hash.hpp
+
+#include "linkedList.hpp"
+
+class HashTable
+{
+private:
+    
+    LinkedList * array;
+    
+    int hash( int itemKey );
+    
+public:
+    
+    HashTable(); // Constructor
+
+    ~HashTable(); // Destructor
+    
+    bool insertItemLin(int toInsert);
+
+    bool insertItemQuad(int toInsert);
+    
+    void printTable();
+    
+};
+
---
+++ hashmain.cpp
@@ -0,0 +1,26 @@
+
+#include "hash.hpp"
+
+int main()
+{
+    int pullFrom; 
+    int tabLen = 0;
+
+    Hashtable table;
+
+    cout << "Please enter up to 10 non-negative integers, followed by -1";
+
+    for(i = 0; i < 11; i++)
+    {
+        scanf(%d, &pullFrom)
+
+        if(pullFrom == -1)
+            break;
+
+        table.insertItemLin(pullFrom)
+
+
+    }
+
+    return 0;
+}
